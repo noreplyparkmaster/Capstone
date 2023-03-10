6f5a0c23d8de3529d7d244b5b6ae7b8be6ce4077
Parkmaster <noreply.parkmaster@gmail.com>
2023-03-10 09:02:15 +0700

    Added Encryption on database, Obfuscation using ProGuard

--- core/src/main/java/com/rhm/capstone/core/di/DatabaseModule.kt
+++ core/src/main/java/com/rhm/capstone/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,10 +19,16 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): GameDatabase = Room.databaseBuilder(
-        context,
-        GameDatabase::class.java, "Game.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): GameDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("ryan_capstone".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            GameDatabase::class.java, "Game.db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideGameDao(database: GameDatabase): GameDao = database.gameDao()
